2c54f1bad7cf14dc682ac47da8a10c9b168e750f
Vishnu2003 <60405417+Vishnu2003@users.noreply.github.com>
2020-12-04 22:36:30 +0530

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -9,9 +9,6 @@ from async_timeout import timeout
 from discord.ext import commands
 import time
 
- 
-
-
 client = commands.Bot('>', description='The UN-SUS BOT (winko wink)')
 
 # Silence useless bug reports messages
@@ -301,7 +298,6 @@ class Music(commands.Cog):
         ctx.voice_state.voice = await destination.connect()
 
     @commands.command(name='summon')
-    @commands.has_permissions(manage_guild=True)
     async def _summon(self, ctx: commands.Context, *, channel: discord.VoiceChannel = None):
         """Summons the bot to a voice channel.
         If no channel was specified, it joins your channel.
@@ -318,7 +314,6 @@ class Music(commands.Cog):
         ctx.voice_state.voice = await destination.connect()
 
     @commands.command(name='leave', aliases=['disconnect'])
-    @commands.has_permissions(manage_guild=True)
     async def _leave(self, ctx: commands.Context):
         """Clears the queue and leaves the voice channel."""
 
@@ -348,7 +343,6 @@ class Music(commands.Cog):
         await ctx.send(embed=ctx.voice_state.current.create_embed())
 
     @commands.command(name='pause')
-    @commands.has_permissions(manage_guild=True)
     async def pause(self, ctx: commands.Context):
         """Pauses the currently playing song."""
 
@@ -357,7 +351,6 @@ class Music(commands.Cog):
             await ctx.message.add_reaction('⏯')
 
     @commands.command(name='resume')
-    @commands.has_permissions(manage_guild=True)
     async def _resume(self, ctx: commands.Context):
         """Resumes a currently paused song."""
 
@@ -366,7 +359,6 @@ class Music(commands.Cog):
             await ctx.message.add_reaction('⏯')
 
     @commands.command(name='stop')
-    @commands.has_permissions(manage_guild=True)
     async def _stop(self, ctx: commands.Context):
         """Stops playing song and clears the queue."""
 
